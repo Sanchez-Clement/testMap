de5dcb7643a2f57f42218cd3290502cb5ff57451
clement <sanchez.clmnt@gmail.com>
2017-12-15 15:07:49 +0100

    ok for style of window conten

--- web/js/main.js
+++ web/js/main.js
@@ -1,11 +1,17 @@
 var map;
 var json;
 var markers = [];
+var iconeFerme;
 $( document ).ready(function() {
+
+
+
+
     $.get( "/js/shops.json", function( data ) {
         json = data;
         initMap();
     });
+
 
 
 
@@ -68,19 +74,23 @@ $( document ).ready(function() {
             },
             title: json[i].name,
             map: map,
-            icon: {
-                path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
-                fillColor: 'blue',
-                fillOpacity: 0.8,
-                scale: 3,
-                strokeColor: 'black',
-                strokeWeight: 1
+            icon:
+            { url: 'img/farm.png',
 
-            },
+                /*strokeColor: 'black'*/
 
 
+                 /*fillColor: 'blue',
+                 fillOpacity: 0.8,
+scale: 3 ,
+
+                 strokeWeight: 1*/
+
+
+            }
 
         });
+
 
 
 
@@ -96,7 +106,19 @@ $( document ).ready(function() {
 
 function attachSecretMessage(marker, secretMessage,id) {
     var infowindow = new google.maps.InfoWindow({
-        content: "<a href=' /shop/" + id + "'>" + secretMessage + "</a>"
+        content:
+    '<div class="card horizontal" id="contentMap">' +
+        '<div class="card-image">' +
+        '<img src="https://lorempixel.com/100/190/nature/6">'+
+        '</div>' +
+        '<div class="card-stacked">' +
+        '<div class="card-content">' +
+        '<p>' + secretMessage + '</p>' +
+    '</div>' +
+
+    '</div>' +
+    '</div>'
+
     });
 
     marker.addListener('click', function() {
@@ -107,7 +129,31 @@ function attachSecretMessage(marker, secretMessage,id) {
     marker.addListener('mouseover', function() {
 
         infowindow.open(map, marker);
+        var iwOuter = $('.gm-style-iw');
+        var iwCloseBtn = iwOuter.next();
+        iwCloseBtn.css({
+            opacity: '0', // by default the close button has an opacity of 0.7
+
+        });
+
+        /* The DIV we want to change is above the .gm-style-iw DIV.
+         * So, we use jQuery and create a iwBackground variable,
+         * and took advantage of the existing reference to .gm-style-iw for the previous DIV with .prev().
+         */
+        var iwBackground = iwOuter.prev();
+
+
+        iwBackground.children(':nth-child(2)').css({'display' : 'none'});
+
+
+        iwBackground.children(':nth-child(4)').css({'display' : 'none'});
     });
+
+
+
+
+
+
 
     marker.addListener('mouseout', function() {
 
@@ -115,7 +161,10 @@ function attachSecretMessage(marker, secretMessage,id) {
     });
 
 
+
+
 }
+
 
 
 
